c410eb19157ed98daea405290da9d84cce32c048
Dusty Phillips <dusty@phillips.codes>
2023-03-27 17:31:09 -0300

    Update pygments

--- .pythonpath/sitecustomize.py
+++ .pythonpath/sitecustomize.py
@@ -1,11 +1,39 @@
 from contextlib import suppress
+from pprint import pprint
 
-with suppress(ImportError):
+__builtins__["pprint"] = pprint
+
+try:
     from devtools import debug
+except ImportError:
 
-    __builtins__["debug"] = debug
+    def debug(*args, file_=None, flush_=True, **kwargs):
+        for arg in args:
+            pprint(arg, file_)
+
 
 with suppress(ImportError):
     import colored_traceback
 
     colored_traceback.add_hook()
+
+
+def djbug(*args, **kwargs):
+    new_args = []
+    for arg in args:
+        if "django.db.models.base.Model" in {
+            f"{cl.__module__}.{cl.__qualname__}" for cl in arg.__class__.__mro__
+        }:
+            new_args.append(
+                (
+                    f"Django Model {arg.__class__.__name__}",
+                    {f.name: getattr(arg, f.name) for f in arg._meta.fields},
+                )
+            )
+        else:
+            new_args.append(arg)
+
+    debug(*new_args, **kwargs)
+
+
+__builtins__["debug"] = djbug
