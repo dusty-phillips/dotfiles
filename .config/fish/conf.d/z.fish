e2e79fcdd8e2a3bc9ebb6b724e454c7d6cb090c7
Dusty Phillips <dusty@phillips.codes>
2024-03-23 12:55:57 -0300

    Try out fish shell

---
+++ .config/fish/conf.d/z.fish
@@ -0,0 +1,1 @@
+lua /usr/local/share/z.lua/z.lua --init fish enhanced once fzf | source
---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+    # Managing dot files (https://www.atlassian.com/git/tutorials/dotfiles)
+    # curl -Lks https://is.gd/B5GXbi | /bin/bash
+    alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+    set fish_greeting
+    set fish_cursor_insert line
+    set -x EDITOR nvim
+
+    fish_vi_key_bindings
+
+    # Need brew install fisher fzf && fisher install PatrickF1/fzf.fish
+    fzf_configure_bindings --directory=\cf --processes=\cp --git_log=\cl --git_status=\cs
+    set fzf_fd_opts --exclude Library
+
+    function fish_user_key_bindings
+        bind --mode insert \er 'z -I . ; commandline -f repaint'
+    end
+
+    alias lg='lazygit'
+    alias rm=grm
+    alias sed=gsed
+    function grep # Needs to be function or fish overrules it
+        ggrep --color=auto $argv
+    end
+
+    starship init fish | source
+end
