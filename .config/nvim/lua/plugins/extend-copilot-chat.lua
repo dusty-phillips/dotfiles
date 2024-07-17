9cf23e328c5ff40a9f581df80dd977b38ae36faf
Dusty Phillips <dusty@phillips.codes>
2024-07-17 14:51:12 -0300

    cancelled my copilot subscription for now

--- .config/nvim/lua/plugins/extend-copilot-chat.lua
+++ .config/nvim/lua/plugins/extend-copilot-chat.lua
@@ -1,21 +1,22 @@
-return {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  keys = {
-    {
-      "<leader>aa",
-      function()
-        return require("CopilotChat").open()
-      end,
-      desc = "CopilotChat",
-      mode = { "n", "v" },
-    },
-    {
-      "<leader>aA",
-      function()
-        return require("CopilotChat").close()
-      end,
-      desc = "Close (CopilotChat)",
-      mode = { "n", "v" },
-    },
-  },
-}
+return {}
+-- return {
+--   "CopilotC-Nvim/CopilotChat.nvim",
+--   keys = {
+--     {
+--       "<leader>aa",
+--       function()
+--         return require("CopilotChat").open()
+--       end,
+--       desc = "CopilotChat",
+--       mode = { "n", "v" },
+--     },
+--     {
+--       "<leader>aA",
+--       function()
+--         return require("CopilotChat").close()
+--       end,
+--       desc = "Close (CopilotChat)",
+--       mode = { "n", "v" },
+--     },
+--   },
+-- }
