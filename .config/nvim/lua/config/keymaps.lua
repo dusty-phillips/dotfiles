500d50088868e486d615ea0dacb1a3e81b95e563
Dusty Phillips <dusty@phillips.codes>
2024-11-06 12:15:16 -0400

    Remove the bufferline. I've spent months trying to get used to it and it's not useful. Had to add a couple keybindings to my base config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-w>N", "<cmd>enew<cr>", { desc = "New file in current window" })
-vim.keymap.set("n", "<leader>wN", "<cmd>enew<cr>", { desc = "New file in current window" })
+vim.keymap.set("n", "<C-w>N", "<cmd>enew<cr>", { desc = "New File in Current Window" })
+vim.keymap.set("n", "<leader>wN", "<cmd>enew<cr>", { desc = "New File in Current Window" })
 vim.keymap.set("n", "<leader>r", "<cmd>LspRestart<cr>", { desc = "Restart LSP" })
-vim.keymap.set("n", "<leader>xc", "<cmd>Trouble close<cr>", { desc = "Close trouble window" })
+vim.keymap.set("n", "<leader>xc", "<cmd>Trouble close<cr>", { desc = "Close Trouble Window" })
 vim.keymap.set("n", "<D-s>", "<cmd>w<cr>", { desc = "Save" })
+vim.keymap.set("i", "<D-s>", "<Esc><cmd>w<cr>a", { desc = "Save" })
+
+vim.keymap.set("n", "<leader><delete>", LazyVim.ui.bufremove, { desc = "Close Buffer" })
+vim.keymap.set("n", "<leader><CR>", "<cmd>%bd<cr>", { desc = "Close All Buffers" })
+vim.keymap.set("n", "<leader>bo", "<cmd>%bd | e# | bd#<cr>", { desc = "Close Other Buffers" })
