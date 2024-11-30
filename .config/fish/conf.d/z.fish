3d8a63890cacdc9fe7b4d8878bbc29eccf656718
Dusty Phillips <dusty@phillips.codes>
2024-11-30 09:46:44 -0400

    Support silicon homebrew and intel homebrew

--- .config/fish/conf.d/z.fish
+++ .config/fish/conf.d/z.fish
@@ -1,1 +1,6 @@
-lua /opt/homebrew/opt/z.lua/share/z.lua/z.lua --init fish enhanced once fzf | source
+
+if test -f /usr/local/share/z.lua/z.lua
+    lua /usr/local/share/z.lua/z.lua --init fish enhanced once fzf | source
+else
+    lua /opt/homebrew/opt/z.lua/share/z.lua/z.lua --init fish enhanced once fzf | source
+end
