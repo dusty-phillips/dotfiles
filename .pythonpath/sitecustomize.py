22410caf5cbb325b4ee8478e7656a27d7be06197
Dusty Phillips <dusty.phillips@floatcard.com>
2024-12-06 10:04:34 -0400

    don't need djbug because devtools debugs django objects nicely now

--- .pythonpath/sitecustomize.py
+++ .pythonpath/sitecustomize.py
@@ -18,23 +18,4 @@ with suppress(ImportError):
     colored_traceback.add_hook()
 
 
-def djbug(*args, **kwargs):
-    new_args = []
-    for arg in args:
-        if "django.db.models.base.Model" in {
-            f"{cl.__module__}.{cl.__qualname__}" for cl in arg.__class__.__mro__
-        }:
-            new_args.append(
-                (
-                    f"Django Model {arg.__class__.__name__}",
-                    {f.name: getattr(arg, f.name) for f in arg._meta.fields},
-                )
-            )
-        else:
-            new_args.append(arg)
-
-    debug(*new_args, **kwargs)
-
-
 __builtins__["debug"] = debug
-__builtins__["djbug"] = djbug
