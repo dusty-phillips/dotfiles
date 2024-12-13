66b15639de19b07a17f564ff080df7e8f460b6c5
Dusty Phillips <dusty.phillips@floatcard.com>
2024-12-13 09:39:38 -0500

    plugins

--- .config/nvim/lua/plugins/neotest-bun.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- { url = "file:///Users/dustyphillips/Desktop/Code/neotest-bun" },
-  { dir = "~/Desktop/Code/neotest-bun/" },
-  {
-    "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-bun" } },
-  },
-}
