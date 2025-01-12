08e64c263a169c4b999fc948711a23ee93c83d08
Dusty Phillips <dusty@phillips.codes>
2025-01-12 11:22:22 -0400

    discovered that alt-a does what I had stolen ctrl-a for

--- .config/nvim/lua/plugins/extend-fzf-lua.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "ibhagwan/fzf-lua",
-  opts = function(_, opts)
-    local fzf = require("fzf-lua")
-    local config = fzf.config
-    config.defaults.keymap.fzf["ctrl-a"] = "select-all"
-    return opts
-  end,
-}
