2cd7e757b0edf57ecfedb072a127e95d6cc06f46
Dusty Phillips <dusty@phillips.codes>
2025-01-16 12:08:02 -0400

    improve git hunk menu

--- .config/nvim/lua/plugins/git-conflict.lua
+++ .config/nvim/lua/plugins/git-conflict.lua
@@ -1,18 +1,42 @@
 return {
-  "akinsho/git-conflict.nvim",
-  lazy = false,
-  opts = {
-    default_mappings = {
-      ours = "<leader>ho",
-      theirs = "<leader>ht",
-      none = "<leader>h0",
-      both = "<leader>hb",
-      next = "]x",
-      prev = "[x",
+  {
+    "akinsho/git-conflict.nvim",
+    lazy = false,
+    opts = {
+      default_mappings = {
+        ours = "<leader>ho",
+        theirs = "<leader>ht",
+        none = "<leader>h0",
+        both = "<leader>hb",
+        next = "]x",
+        prev = "[x",
+      },
+    },
+    keys = {
+      { "<leader>hx", "<cmd>GitConflictListQf<cr>", desc = "List Conflicts" },
+      { "<leader>hX", "<cmd>GitConflictRefresh<cr>", desc = "Refresh Conflicts" },
+      {
+        "<leader>hb",
+        function()
+          package.loaded.gitsigns.blame_line({ full = true })
+        end,
+        desc = "Blame Line",
+      },
+      {
+        "<leader>hB",
+        function()
+          package.loaded.gitsigns.blame()
+        end,
+        desc = "Blame Buffer",
+      },
     },
   },
-  keys = {
-    { "<leader>gx", "<cmd>GitConflictListQf<cr>", desc = "List Conflicts" },
-    { "<leader>gr", "<cmd>GitConflictRefresh<cr>", desc = "Refresh Conflicts" },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { mode = { "n", "v" }, { "<leader>h", group = "Git Hunk", icon = "  " } },
+      },
+    },
   },
 }
