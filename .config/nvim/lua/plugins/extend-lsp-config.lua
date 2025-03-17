e63b2f7ddbc7bc925c5b58225e207eab1063e7b3
Dusty Phillips <dusty@phillips.codes>
2025-03-17 09:13:39 -0300

    minor tweaks

--- .config/nvim/lua/plugins/extend-lsp-config.lua
+++ .config/nvim/lua/plugins/extend-lsp-config.lua
@@ -2,6 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = { enabled = false },
       servers = {
         css_variables = {
           filetypes = { "css", "scss", "less", "svelte" },
