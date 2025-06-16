c9a00c7ca87d44985f900ac3a25afacc3d0edfac
Dusty Phillips <dusty@phillips.codes>
2025-06-16 13:04:38 -0300

    grab the codecompanion config from a closed lazyvim pr

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -1,44 +1,47 @@
 return {
   {
+
     "olimorris/codecompanion.nvim",
-    opts = {
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        {
-          "saghen/blink.cmp",
-          opts = {
-            sources = {
-              default = { "codecompanion" },
-              providers = {
-                codecompanion = {
-                  name = "CodeCompanion",
-                  module = "codecompanion.providers.completion.blink",
-                  enabled = true,
-                },
-              },
-            },
-          },
-        },
-      },
-      strategies = {
-        chat = {
-          adapter = "copilot",
-          model = "claude-sonnet-4-20250514",
-        },
-      },
-    },
+    cmd = { "CodeCompanion", "CodeCompanionActions", "CodeCompanionToggle", "CodeCompanionAdd", "CodeCompanionChat" },
+
+    opts = function()
+      local options = require("codecompanion.config")
+      local user = vim.env.USER or "You"
+
+      options.strategies.chat.roles = {
+        llm = "  CodeCompanion",
+        user = "  " .. user,
+      }
+
+      options.strategies.chat.keymaps.close.modes = {
+        n = "q",
+        i = "<Esc>",
+      }
+      options.strategies.chat.keymaps.stop.modes.n = "<Esc>"
+      options.strategies.chat.keymaps.send.modes.n = "<CR>"
+
+      return options
+    end,
+
     keys = {
-      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Code Companion Actions" },
-      { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "Code Companion Chat" },
+      { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+      { "<leader>ap", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Prompt Actions (CodeCompanion)" },
+      { "<leader>aa", "<cmd>CodeCompanionToggle<cr>", mode = { "n", "v" }, desc = "Toggle (CodeCompanion)" },
+      { "<leader>ac", "<cmd>CodeCompanionAdd<cr>", mode = "v", desc = "Add code to CodeCompanion" },
+      { "<leader>ai", "<cmd>CodeCompanion<cr>", mode = "n", desc = "Inline prompt (CodeCompanion)" },
     },
   },
+
   {
-    "folke/which-key.nvim",
-    opts = {
-      spec = {
-        { mode = { "n", "v" }, { "<leader>h", group = "Git Hunk", icon = "  " } },
-      },
-    },
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        ft = "codecompanion",
+        title = "CodeCompanion Chat",
+        size = { width = 50 },
+      })
+    end,
   },
 }
